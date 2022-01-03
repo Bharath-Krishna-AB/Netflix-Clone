ddb4ac268692cee3da6e6cb12985ca74f3cb4ea9
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 16:51:07 +0530

    navbar skeleton completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Navbar() {
     return (
         <nav className='Navbar'>
-            
+            <div className='left-side'>
+            <a className='Logo-Link' href="#Home">
+            <img  className='logo' src="Assets/images/Netflix-Logo.png" alt="" />
+            </a>
+            <div className="section-links">
+                <a className='links' href="#Netflix-Originals">Netflix Originals</a>
+                <a className='links' href="#Action">Action</a>
+                <a className='links' href="#Drama">Drama</a>
+                <a className='links' href="#Thriller">Thriller</a>
+                <a className='links' href="#Horror">Horror</a>
+
+            </div>
+            </div>
+            <img className='avatar' src="assets/images/avatar.png" alt="" /> 
         </nav>
     )
 }

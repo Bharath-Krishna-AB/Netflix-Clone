b98e3363c419ee9e3eaa3fb93b9d9ca5d7acc1ce
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 23:22:19 +0530

    created videoarea route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import VideoArea from './Pages/VideoArea';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/video/:id' element={<VideoArea/>}/>
+
       </Routes>
 
     </div>
---
+++ src/Pages/VideoArea.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Fragment } from 'react/cjs/react.production.min'
+import VideoPlayer from '../components/VideoPlayer/VideoPlayer'
+
+function VideoArea() {
+    return (
+        <Fragment>
+            <VideoPlayer/>
+        </Fragment>
+    )
+}
+
+export default VideoArea

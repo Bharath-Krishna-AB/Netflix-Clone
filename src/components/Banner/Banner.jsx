69727b713b1d70cc8cef188e98706075219acbe8
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 17:02:15 +0530

    Banner skeleton added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Banner() {
     return (
         <section className='Banner'>
-
+            <div className="content">
+                <h1>Movie Title</h1>
+                <div className="banner-buttons">
+                    <button className="buttons">Play</button>
+                    <button className="buttons">My list</button>
+                </div>
+                <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, dolore quae? Exercitationem tempore quasi in sequi.</p>
+            </div>
         </section>
     )
 }

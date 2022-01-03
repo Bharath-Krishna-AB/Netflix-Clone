dc940b5b117bc31b4da58eac695c9993675b07dd
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 23:07:27 +0530

    firebase initialized

---
+++ src/Firebase/config.js
@@ -0,0 +1,14 @@
+import {initializeApp} from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDt-y909EIP4cWwtFCjnP_-MbQoWTJWh14",
+    authDomain: "netflix-project-1e76b.firebaseapp.com",
+    projectId: "netflix-project-1e76b",
+    storageBucket: "netflix-project-1e76b.appspot.com",
+    messagingSenderId: "132863871338",
+    appId: "1:132863871338:web:331c2e635ae43d0853bd13",
+    measurementId: "G-6ES655K2DR"
+  };
+
+  export const firebase = initializeApp(firebaseConfig)

97c8411ee000e5315c7d609b11a6f525222ed7ef
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 17:30:48 +0530

    bannerobj created

---
+++ src/movie-objectDatas/BannerObj.js
@@ -0,0 +1,121 @@
+
+
+ export const BannerData = [
+    {
+        'backdrop_path': "/Assets/Images/Kurup.jpg",
+        "title": "Kurup",
+        "overview": "Sukumara Kurup - a consummate criminal sets out to find a man of similar appearance to falsify his own death and claim insurance money.",
+        "videoUrl":  "/Assets/video/Kurup_trailer.mp4",
+        'poster_path': '/Assets/Images/Kurup_Poster.jpg',
+        'id' : '0',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Minnal-Murali.jpg",
+        "title": "Minnal Murali",
+        "overview": "A tailor gains special powers after being struck by lightning, but must take down an unexpected foe if he is to become the superhero his hometown in Kerala needs.",
+        "videoUrl":  "/Assets/video/Minnal_Murali_trailer.mp4",
+        'poster_path': '/Assets/Images/Minnal-Murali_Poster.jpg',
+        'id' : '1',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Marakkar.jpg",
+        "title": "Marakkar: Lion of the Arabian Sea",
+        "overview": "‘Marakkar Lion of the Arabian Sea’ portrays the courageous life-events of a rebellious naval chief, Kunjali Marakkar the fourth, who fought against the Portuguese in the ancient times. He was the fourth naval chief of the Calicut Zamorin. The film revolves around him who was also the first Indian Naval Commander and Indian freedom fighter for the war against the Portuguese. He is said to have won 16 such battles with his impeccable strategies and fighting skills.",
+        "videoUrl":  "/Assets/video/Marakkar_trailer.mp4",
+        'poster_path': '/Assets/Images/Marakkar_Poster.jpg',
+        'id' : '2',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Pushpa.jpg",
+        "title": "Pushpa: The Rise – Part 1",
+        "overview": "Pushpa Raj is a coolie who rises in the world of red sandalwood smuggling. Along the way, he doesn’t shy from making an enemy or two.",
+        "videoUrl":  "/Assets/video/Pushpa_trailer.mp4",
+        'poster_path': '/Assets/Images/Pushpa_Poster.jpg',
+        'id' : '3',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/RRR.jpg",
+        "title": "RRR",
+        "overview": "A fictional tale about two legendary Indian revolutionaries and their journey away from home before they started fighting for their country in 1920s.",
+        "videoUrl":  "/Assets/video/RRR_trailer.mp4",
+        'poster_path': '/Assets/Images/RRR_Poster.jpg',
+        'id' : '4',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Kaaval.jpg",
+        "title": "Kaaval",
+        "overview": "Thampan and Antony are a long time best friends and business partners who owned a saw mill at Kattappana in the past. A family tragedy and its falling-out separates them for good. But now Antony and his family faces a grievous situation, and he is forced to ask his old friend for a help out. The timeline of the story spans over two generations.",
+        "videoUrl":  "/Assets/video/Kaaval_trailer.mp4",
+        'poster_path': '/Assets/Images/Kaaval_Poster.jpg',
+        'id' : '5',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Jan-e-Man.jpg",
+        "title": "Jan-e-Man",
+        "overview": "The hilarious comic twists that awaits when Joymon, desperate to escape his lonely, mundane and routine life in remote freezing Canada, decides to travel to his hometown in Kerala only to celebrate his 30th birthday, forms the base of the story.",
+        "videoUrl":  "/Assets/video/Jan-e-Man_trailer.mp4",
+        'poster_path': '/Assets/Images/Jan-e-man_Poster.jpg',
+        'id' : '6',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/No_Way_Home.jpg",
+        "title": "Spider-Man: No Way Home",
+        "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+        "videoUrl":  "/Assets/video/No_Way_Home_trailer.mp4",
+        'poster_path': '/Assets/Images/No_Way_Home_Poster.jpg',
+        'id' : '7',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Kgf_2.jpg",
+        "title": "K.G.F: Chapter 2",
+        "overview": "Rocky, whose name strikes fear in the heart of his foes. His allies look up to Rocky as their Savior, the government sees him as a threat to law and order; enemies are clamoring for revenge and conspiring for his downfall. Bloodier battles and darker days await as Rocky continues on his quest for unchallenged supremacy.",
+        "videoUrl":  "/Assets/video/Kgf_2_trailer.mp4",
+        'poster_path': '/Assets/Images/Kgf_2_Poster.jpg',
+        'id' : '8',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Cold_Case.jpg",
+        "title": "Cold Case",
+        "overview": "Sathyajith, Assistant Commissioner of Police, along with his team is assigned to handle controversial and complicated cases. One day, while fishing on a lake side, a man finds a garbage bag trapped inside his net.",
+        "videoUrl":  "/Assets/video/Cold_Case_trailer.mp4",
+        'poster_path': '/Assets/Images/Cold_Case_Poster.jpg',
+        'id' : '9',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Shershaah.jpg",
+        "title": "Shershaah",
+        "overview": "Inspired by the life of Captain Vikram Batra (PVC), the film celebrates his bravery, valiant spirit and honors his invaluable sacrifice during the Kargil War of 1999, at the age of 24.",
+        "videoUrl":  "/Assets/video/Shershaah_trailer.mp4",
+        'poster_path': '/Assets/Images/Shershaah_Poster.jpg',
+        'id' : '10',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/The-Priest.jpg",
+        "title": "The Priest",
+        "overview": "The culprits behind multiple deaths in a family over a few years are exposed, but there is more to it than meets the eye.",
+        "videoUrl":  "/Assets/video/The-Priest_trailer.mp4",
+        'poster_path': '/Assets/Images/The-Priest_Poster.jpg',
+        'id' : '11',
+        'mylist' : 'far'
+    },
+    {
+        'backdrop_path': "/Assets/Images/Jai-Bhim.jpg",
+        "title": "Jai Bhim",
+        "overview": "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. So as to find her husband and seek justice for them, as their voice, a High Court advocate rises in support. Will their battle for justice succeed?",
+        "videoUrl":  "/Assets/video/Jai-Bhim_trailer.mp4",
+        'poster_path': '/Assets/Images/Jai-Bhim_Poster.jpg',
+        'id' : '12',
+        'mylist' : 'far'
+    },
+]

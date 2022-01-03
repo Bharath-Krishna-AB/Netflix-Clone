138818d96409bb4231a3d214a0c460f81d5bdd6f
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-03 17:37:53 +0530

    rendered banner data dynamic

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BannerData } from '../../movie-objectDatas/BannerObj'
 import './Banner.css'
 
 function Banner() {
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        setMovie(BannerData[Math.floor(Math.random() * BannerData.length)])
+    }, [])
+
+
     return (
-        <section className='Banner' style={{backgroundImage: `url(https://images.indianexpress.com/2020/01/kurup-first-look-dq-1200.jpg)`}}>
+        <section className='Banner' style={{backgroundImage: `url(${movie.backdrop_path})`}}>
             <div className="content">
-                <h1>Movie Title</h1>
+                <h1>{movie.title}</h1>
                 <div className="banner-buttons">
                     <button className="buttons">Play</button>
                     <button className="buttons">My list</button>
                 </div>
-                <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, dolore quae? Exercitationem tempore quasi in sequi.</p>
+                <p className="description">{movie.overview}</p>
             </div>
         </section>
     )

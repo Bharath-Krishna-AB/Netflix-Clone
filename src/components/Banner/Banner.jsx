6d1a7c9ae5e9e71de1d05947276abc80373994d7
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-04 17:54:30 +0530

    video playing on clicking play button enabled

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { BannerData } from '../../movie-objectDatas/BannerObj'
 import './Banner.css'
 
 function Banner() {
+    const navigate = useNavigate()
     const [movie, setMovie] = useState([])
 
     useEffect(() => {
@@ -15,7 +17,9 @@ function Banner() {
             <div className="content">
                 <h1>{movie.title}</h1>
                 <div className="banner-buttons">
-                    <button className="buttons">Play</button>
+                    <button className="buttons" onClick={()=>{
+                        navigate(`/video/${movie.id}`)
+                    }}>Play</button>
                     <button className="buttons">My list</button>
                 </div>
                 <p className="description">{movie.overview}</p>
--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { BannerData } from '../../movie-objectDatas/BannerObj'
 
 function VideoPlayer() {
     const navigate = useNavigate()
+    const [video, setVideo] = useState('')
+    const {id} = useParams()
+
+    useEffect(() => {
+        setVideo(BannerData[id].videoUrl)
+    }, [])
     return (
         <section className="VidePlayer">
               <div  className="goBack-container">
@@ -10,7 +17,8 @@ function VideoPlayer() {
                     navigate('/')
                 }} className="arrow fas fa-arrow-left"></i>
             </div>
-     <video className='VideoPlayer'  controls autoPlay src={}></video> 
+     <video className='VideoPlayer'  controls autoPlay src={video}></video> 
+     
         </section>
     )
 }

9f1b4999a0954bc1855ae9273e357a9c53362d1d
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-04 21:51:49 +0530

    created axios instance

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,36 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { BannerData } from '../../movie-objectDatas/BannerObj'
 import './Poster.css'
 
-function Poster({title,orignals,genreData}) {
+function Poster({title,originals,genreData}) {
     const navigate = useNavigate()
     const [movie, setMovie] = useState([])
 
     useEffect(() => {
-        if(orignals){
         setMovie(genreData)
-        }else{
-            
-        }
     }, [])
-
 
     return (
         <section className="Poster" id={title}>
        <h2 className="Poster-title">{title}</h2>
 
          <div className="posters">
-             {
-                 movie.map((data)=>{
+             
+             {movie.map((data)=>{
                      return(
-                        <img className='card' onClick={()=>{
+                        <img key={data.id} className='card' onClick={()=>{
                             navigate(`/video/${data.id}`)
                         }} src={data.poster_path} alt="" />
                      )
 
-                 })
-             }
+                 })}
            </div>
         </section>
     )

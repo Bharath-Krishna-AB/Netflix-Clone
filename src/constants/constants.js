91618c7c5123a9de0e0dc60f6ee42e300142c34a
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-04 20:35:17 +0530

    added constants

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '5f87c62e30b3d136fff727e185c18c68'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/original/'
+export const POSTER_IMAGE_URL = 'https://image.tmdb.org/t/p/w300/'
+
+export const ACTION = `/discover/movie?api_key=${API_KEY}&with_genres=28`

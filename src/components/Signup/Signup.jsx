6ee826aba2410370885d9ed4d934a315415aec26
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-23 11:20:08 +0530

    signup page skelteon added

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -7,6 +7,14 @@ function Signup() {
           <img  className='Signuplogo' src="Assets/images/Netflix-Logo.png" alt="" />
           <img className='background-Banner-Netflix' src="Assets/images/background-Banner-Netflix.jpg" alt="" />
           <div className="Signup-Container">
+              <h1 className='SignIn-heading' >Sign In</h1>
+              <div className="form-container">
+                  <form action="">
+                      <input className='input-field' type="text" placeholder='Email or phone number' />
+                      <input className='input-field' type="text" placeholder='Password' />
+                      <input type="submit" />
+                  </form>
+              </div>
           </div>
       </section>
   )

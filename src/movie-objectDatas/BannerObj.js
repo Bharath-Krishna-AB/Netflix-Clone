25cca9dac7824babffba9bf47b0ae991cb5b6dc0
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-03-26 20:55:21 +0530

    added beeshma parvam data

--- src/movie-objectDatas/BannerObj.js
+++ src/movie-objectDatas/BannerObj.js
@@ -2,12 +2,21 @@
 
  export const BannerData = [
     {
+        'backdrop_path': "/Assets/Images/Beeshma.jpg",
+        "title": "Bheeshma Parvam",
+        "overview": "Set in the backdrop of 1980s, Micheal helms his influential family in Kochi, inspiring fear and respect through his unnerving past. When a few younger members in the household feel his power over their lives too are aggravating, they join hands with foes to clamp down on him. And the story follows what’s in store for them.",
+        "videoUrl":  "/Assets/video/Bheeshma_Parvam_trailer.mp4",
+        'poster_path': '/Assets/Images/Beeshma_Poster.jpg',
+        'id' : '0',
+        'mylist' : 'far'
+    },
+    {
         'backdrop_path': "/Assets/Images/Kurup.jpg",
         "title": "Kurup",
         "overview": "Sukumara Kurup - a consummate criminal sets out to find a man of similar appearance to falsify his own death and claim insurance money.",
         "videoUrl":  "/Assets/video/Kurup_trailer.mp4",
         'poster_path': '/Assets/Images/Kurup_Poster.jpg',
-        'id' : '0',
+        'id' : '1',
         'mylist' : 'far'
     },
     {
@@ -16,7 +25,7 @@
         "overview": "A tailor gains special powers after being struck by lightning, but must take down an unexpected foe if he is to become the superhero his hometown in Kerala needs.",
         "videoUrl":  "/Assets/video/Minnal_Murali_trailer.mp4",
         'poster_path': '/Assets/Images/Minnal-Murali_Poster.jpg',
-        'id' : '1',
+        'id' : '2',
         'mylist' : 'far'
     },
     {
@@ -25,7 +34,7 @@
         "overview": "‘Marakkar Lion of the Arabian Sea’ portrays the courageous life-events of a rebellious naval chief, Kunjali Marakkar the fourth, who fought against the Portuguese in the ancient times. He was the fourth naval chief of the Calicut Zamorin. The film revolves around him who was also the first Indian Naval Commander and Indian freedom fighter for the war against the Portuguese. He is said to have won 16 such battles with his impeccable strategies and fighting skills.",
         "videoUrl":  "/Assets/video/Marakkar_trailer.mp4",
         'poster_path': '/Assets/Images/Marakkar_Poster.jpg',
-        'id' : '2',
+        'id' : '3',
         'mylist' : 'far'
     },
     {
@@ -34,7 +43,7 @@
         "overview": "Pushpa Raj is a coolie who rises in the world of red sandalwood smuggling. Along the way, he doesn’t shy from making an enemy or two.",
         "videoUrl":  "/Assets/video/Pushpa_trailer.mp4",
         'poster_path': '/Assets/Images/Pushpa_Poster.jpg',
-        'id' : '3',
+        'id' : '4',
         'mylist' : 'far'
     },
     {
@@ -43,7 +52,7 @@
         "overview": "A fictional tale about two legendary Indian revolutionaries and their journey away from home before they started fighting for their country in 1920s.",
         "videoUrl":  "/Assets/video/RRR_trailer.mp4",
         'poster_path': '/Assets/Images/RRR_Poster.jpg',
-        'id' : '4',
+        'id' : '5',
         'mylist' : 'far'
     },
     {
@@ -52,7 +61,7 @@
         "overview": "Thampan and Antony are a long time best friends and business partners who owned a saw mill at Kattappana in the past. A family tragedy and its falling-out separates them for good. But now Antony and his family faces a grievous situation, and he is forced to ask his old friend for a help out. The timeline of the story spans over two generations.",
         "videoUrl":  "/Assets/video/Kaaval_trailer.mp4",
         'poster_path': '/Assets/Images/Kaaval_Poster.jpg',
-        'id' : '5',
+        'id' : '6',
         'mylist' : 'far'
     },
     {
@@ -61,7 +70,7 @@
         "overview": "The hilarious comic twists that awaits when Joymon, desperate to escape his lonely, mundane and routine life in remote freezing Canada, decides to travel to his hometown in Kerala only to celebrate his 30th birthday, forms the base of the story.",
         "videoUrl":  "/Assets/video/Jan-e-Man_trailer.mp4",
         'poster_path': '/Assets/Images/Jan-e-man_Poster.jpg',
-        'id' : '6',
+        'id' : '7',
         'mylist' : 'far'
     },
     {
@@ -70,7 +79,7 @@
         "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
         "videoUrl":  "/Assets/video/No_Way_Home_trailer.mp4",
         'poster_path': '/Assets/Images/No_Way_Home_Poster.jpg',
-        'id' : '7',
+        'id' : '8',
         'mylist' : 'far'
     },
     {
@@ -79,7 +88,7 @@
         "overview": "Rocky, whose name strikes fear in the heart of his foes. His allies look up to Rocky as their Savior, the government sees him as a threat to law and order; enemies are clamoring for revenge and conspiring for his downfall. Bloodier battles and darker days await as Rocky continues on his quest for unchallenged supremacy.",
         "videoUrl":  "/Assets/video/Kgf_2_trailer.mp4",
         'poster_path': '/Assets/Images/Kgf_2_Poster.jpg',
-        'id' : '8',
+        'id' : '9',
         'mylist' : 'far'
     },
     {
@@ -88,7 +97,7 @@
         "overview": "Sathyajith, Assistant Commissioner of Police, along with his team is assigned to handle controversial and complicated cases. One day, while fishing on a lake side, a man finds a garbage bag trapped inside his net.",
         "videoUrl":  "/Assets/video/Cold_Case_trailer.mp4",
         'poster_path': '/Assets/Images/Cold_Case_Poster.jpg',
-        'id' : '9',
+        'id' : '10',
         'mylist' : 'far'
     },
     {
@@ -97,7 +106,7 @@
         "overview": "Inspired by the life of Captain Vikram Batra (PVC), the film celebrates his bravery, valiant spirit and honors his invaluable sacrifice during the Kargil War of 1999, at the age of 24.",
         "videoUrl":  "/Assets/video/Shershaah_trailer.mp4",
         'poster_path': '/Assets/Images/Shershaah_Poster.jpg',
-        'id' : '10',
+        'id' : '11',
         'mylist' : 'far'
     },
     {
@@ -106,7 +115,7 @@
         "overview": "The culprits behind multiple deaths in a family over a few years are exposed, but there is more to it than meets the eye.",
         "videoUrl":  "/Assets/video/The-Priest_trailer.mp4",
         'poster_path': '/Assets/Images/The-Priest_Poster.jpg',
-        'id' : '11',
+        'id' : '12',
         'mylist' : 'far'
     },
     {
@@ -115,7 +124,7 @@
         "overview": "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. So as to find her husband and seek justice for them, as their voice, a High Court advocate rises in support. Will their battle for justice succeed?",
         "videoUrl":  "/Assets/video/Jai-Bhim_trailer.mp4",
         'poster_path': '/Assets/Images/Jai-Bhim_Poster.jpg',
-        'id' : '12',
+        'id' : '13',
         'mylist' : 'far'
     },
     {
@@ -124,7 +133,7 @@
         "overview": "A young filmmaker is in a soup right when he’s about to find his footing in the film industry. When he sets out to find an answer, turns out it’s in the past.",
         "videoUrl":  "/Assets/video/Shyam_Singha_Roy_trailer.mp4",
         'poster_path': '/Assets/Images/Shyam_Singha_Roy_Poster.jpg',
-        'id' : '13',
+        'id' : '14',
         'mylist' : 'far'
     },
     {
@@ -133,7 +142,7 @@
         "overview": "DCP Aditya, a celebrated cop, faces his most unsettling case yet involving a mysterious serial killer who has been brutally killing his targets and leaving behind notes challenging Aditya to nab him. Teaming with Apoorva, an aspiring crime novelist, and the police force, Aditya finds himself walking on thin ice upon discovering the killer has another challenge for him: to prevent four more murders from happening or give up his medals and resign. What he is yet to find out is the killer's past and his desperation to carry out the killing spree without fail.",
         "videoUrl":  "/Assets/video/V_trailer.mp4",
         'poster_path': '/Assets/Images/V_Poster.jpg',
-        'id' : '14',
+        'id' : '15',
         'mylist' : 'far'
     },
     {
@@ -142,7 +151,7 @@
         "overview": "An alcoholic professor is sent to a juvenile school, where he clashes with a gangster, who uses the children of the school for criminal activities.",
         "videoUrl":  "/Assets/video/Master_trailer.mp4",
         'poster_path': '/Assets/Images/Master_Poster.jpg',
-        'id' : '15',
+        'id' : '16',
         'mylist' : 'far'
     },
     {
@@ -151,7 +160,7 @@
         "overview": "The plot revolves around the life of a family man. Unexpected events happen in their house. The brother and father decide to take on the challenges.",
         "videoUrl":  "/Assets/video/Bro_Daddy_trailer.mp4",
         'poster_path': '/Assets/Images/Bro_Daddy_Poster.jpg',
-        'id' : '16',
+        'id' : '17',
         'mylist' : 'far'
     },
     {
@@ -160,7 +169,7 @@
         "overview": "Khalid, is entrusted with the task of eliminating Kabir, a former soldier turned rogue, as he engages in an epic battle with his mentor who had taught him everything",
         "videoUrl":  "/Assets/video/War_trailer.mp4",
         'poster_path': '/Assets/Images/War_Poster.jpg',
-        'id' : '17',
+        'id' : '18',
         'mylist' : 'far'
     },
     {
@@ -169,7 +178,7 @@
         "overview": "A battle for power ensues as warring gangters thrive to gain possession of a \"black box\" that can make them richer than they already are, and an undercover cop intervenes with the help of his sharp intellect and an instinct to kill.",
         "videoUrl":  "/Assets/video/Saaho_trailer.mp4",
         'poster_path': '/Assets/Images/Saaho_Poster.jpg',
-        'id' : '18',
+        'id' : '19',
         'mylist' : 'far'
     },
     {
@@ -178,7 +187,7 @@
         "overview": "A pianist pretends to be blind for inspiration, witnesses a murder. He suddenly finds himself in a blind spot with the victim's wife, the murderer. During this ordeal, he loses his eyesight for real. Will his girlfriend believe him?",
         "videoUrl":  "/Assets/video/Bhramam_trailer.mp4",
         'poster_path': '/Assets/Images/Bhramam_Poster.jpg',
-        'id' : '19',
+        'id' : '20',
         'mylist' : 'far'
     },
     {
@@ -187,7 +196,7 @@
         "overview": "A fearless, faithful albeit slightly forgetful Mumbai cop, Veer Sooryavanshi, the chief of the Anti-Terrorism Squad in India pulls out all the stops and stunts to thwart a major conspiracy to attack his city.",
         "videoUrl":  "/Assets/video/Sooryavanshi_trailer.mp4",
         'poster_path': '/Assets/Images/Sooryavanshi_Poster.jpg',
-        'id' : '20',
+        'id' : '21',
         'mylist' : 'far'
     },
     {
@@ -196,7 +205,7 @@
         "overview": "Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.",
         "videoUrl":  "/Assets/video/Shang-Chi_trailer.mp4",
         'poster_path': '/Assets/Images/Shang-Chi_Poster.jpg',
-        'id' : '21',
+        'id' : '22',
         'mylist' : 'far'
     },
     {
@@ -205,7 +214,7 @@
         "overview": "After the events of Avengers: Endgame, Dr. Stephen Strange continues his research on the Time Stone. But an old friend turned enemy seeks to destroy every sorcerer on Earth, messing with Strange’s plan and also causing him to unleash an unspeakable evil.",
         "videoUrl":  "/Assets/video/Doctor_Strange_trailer.mp4",
         'poster_path': '/Assets/Images/Doctor_Strange_Poster.jpg',
-        'id' : '22',
+        'id' : '23',
         'mylist' : 'far'
     },
 ]
